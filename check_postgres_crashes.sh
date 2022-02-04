c8af69d9a14c0928098ffe9f099754bd24867d60
Dennis Felsing <dfelsing@yugabyte.com>
2022-02-04 13:00:37 +0000

    Add check_postgres_crashes

---
+++ check_postgres_crashes.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Signal 6 is TRAP, handled by check_trap.sh
+#grep -ar "was terminated by signal" ~/var/data |grep -v "signal 6" > ~/sig
+sed -e "s/.*(PID \([0-9]*\)).*/\1/" ~/sig | while read i; do
+  echo "CHECKING $i" && \
+  gzip -d ~/var/data/pg_data/core.$i.gz && \
+  gdb $HOME/code/yugabyte-db/build/release-clang12-linuxbrew-dynamic-ninja/postgres/bin/postgres \
+    $HOME/var/data/pg_data/core.$i -batch && \
+  gzip -1 $HOME/var/data/pg_data/core.$i
+done

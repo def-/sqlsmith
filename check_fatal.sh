d821d5a96f7eb344443d712382e5ab8d4a0a8937
Dennis Felsing <dennis@felsin9.de>
2022-10-06 12:07:40 +0000

    Use ~/yugabyte-data

--- check_fatal.sh
+++ check_fatal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in ~/var/data/yb-data/tserver/logs/yb-tserver.FATAL.details*; do head -n1 $i; done |
+find ~/yugabyte-data -name '*FATAL.details*' -print | while read line; do head -n1 $i; done |
 grep -v "Check failed: IsFinished()" | # https://github.com/yugabyte/yugabyte-db/issues/11301
 grep -v "Timeout to wait resolve to complete" | # https://github.com/yugabyte/yugabyte-db/issues/11302
 egrep -v "Destructing LookupDataGroup(.*), running_request_number: .* with non empty lookups:" | # https://github.com/yugabyte/yugabyte-db/issues/11308
--- check_trap.sh
+++ check_trap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo ~/var/data/yb-data/tserver/logs/postgresql*.log | xargs cat | grep "TRAP: " |
+find ~/yugabyte-data -name 'postgresql*.log' -print0 | xargs -0 cat | grep "TRAP: " |
 grep -v 'FailedAssertion("!(bms_is_subset(appendrel->lateral_relids, required_outer))", File: "../../../../../../../src/postgres/src/backend/optimizer/util/relnode.c", Line:' | # https://github.com/yugabyte/yugabyte-db/issues/11233
 grep -v 'FailedAssertion("!(!((allPgXact\[proc->pgprocno\].xid) != ((TransactionId) 0)))", File: "../../../../../../../src/postgres/src/backend/storage/ipc/procarray.c", Line:' | # https://github.com/yugabyte/yugabyte-db/issues/11235
 grep -v 'BadArgument("!(((context) != ((void\*)0) && (((((const Node\*)((context)))->type) == T_AllocSetContext) \|\| ((((const Node\*)((context)))->type) == T_SlabContext) \|\| ((((const Node\*)((context)))->type) == T_GenerationContext))))", File: "../../../../../../../src/postgres/src/' | # https://github.com/yugabyte/yugabyte-db/issues/11250

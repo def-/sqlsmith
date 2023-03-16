5349bfa820deb6f5573f05e70783c2cce1189b05
Dennis Felsing <dennis@felsin9.de>
2023-03-16 19:01:16 +0100

    Disable cluster creation for now

--- postgres.cc
+++ postgres.cc
@@ -428,24 +428,24 @@ void dut_libpq::connect(std::string &conninfo)
     command("set client_min_messages to 'ERROR';");
     command("set application_name to '" PACKAGE "::dut';");
 
-    static bool created_cluster = false;
-    static int seed;
-    if (!created_cluster) {
-      ostringstream sseed;
-      sseed << smith::rng;
-      seed = stoi(sseed.str());
-      string cmd = "drop cluster if exists sqlsmith";
-      cmd += to_string(seed);
-      cmd += " cascade";
-      command(cmd);
-      cmd = "create cluster sqlsmith";
-      cmd += to_string(seed);
-      cmd += " replicas (r1 (size = '1'))";
-      command(cmd);
-    }
-    string cmd = "set cluster to sqlsmith";
-    cmd += to_string(seed);
-    command(cmd);
+    //static bool created_cluster = false;
+    //static int seed;
+    //if (!created_cluster) {
+    //  ostringstream sseed;
+    //  sseed << smith::rng;
+    //  seed = stoi(sseed.str());
+    //  string cmd = "drop cluster if exists sqlsmith";
+    //  cmd += to_string(seed);
+    //  cmd += " cascade";
+    //  command(cmd);
+    //  cmd = "create cluster sqlsmith";
+    //  cmd += to_string(seed);
+    //  cmd += " replicas (r1 (size = '1'))";
+    //  command(cmd);
+    //}
+    //string cmd = "set cluster to sqlsmith";
+    //cmd += to_string(seed);
+    //command(cmd);
 
     PQsetNoticeReceiver(conn, dut_libpq_notice_rx, (void *) 0);
 }

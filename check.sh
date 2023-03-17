3210353fde5d1a35466c49e6aa254ee3e646e334
Dennis Felsing <dennis@felsin9.de>
2023-03-17 09:06:16 +0100

    Remove workarounds for 17899 and 17807

--- check.sh
+++ check.sh
@@ -58,13 +58,10 @@ grep -v "operator does not exist: map" | # Should fix, but only happens rarely
 grep -v "result exceeds max size of" | # Seems expected with huge queries
 grep -v "expected expression, but found reserved keyword" | # Should fix, but only happens rarely with subqueries
 grep -v "Expected right parenthesis, found left parenthesis" | # Should fix, but only happens rarely with cast+coalesce
-grep -v "coalesce could not convert type character" | # https://github.com/MaterializeInc/materialize/issues/17899
-grep -v "coalesce could not convert type \"char\"" | # https://github.com/MaterializeInc/materialize/issues/17899
 grep -v "invalid selection: operation may only refer to user-defined tables" | # Seems expected
 #grep -v "cannot reference pseudo type" | # https://github.com/MaterializeInc/materialize/issues/17870
 grep -v "Invalid data in source, saw retractions" | # https://github.com/MaterializeInc/materialize/issues/17874
 #grep -v "internal error: unimplemented join" | # https://github.com/MaterializeInc/materialize/issues/17897
-grep -v "Evaluation error: internal error: invalid input type" | # https://github.com/MaterializeInc/materialize/issues/17807
 grep -v "' not recognized" | # https://github.com/MaterializeInc/materialize/issues/17981
 grep -v "internal error: Invalid data in source, saw negative accumulation for key" | # https://github.com/MaterializeInc/materialize/issues/17509
 grep -v "internal transform error: scalar types do not match" | # https://github.com/MaterializeInc/materialize/issues/18023
